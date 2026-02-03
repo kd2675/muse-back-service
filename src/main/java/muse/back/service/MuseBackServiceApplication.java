fb79e13746c7c588df71781c73b9ab3273379746
김도영 <zz01064842675@gmail.com>
2026-02-03 10:07:31 +0900

    feat: 예외 처리 및 데이터 소스 라우팅 구성 추가
    - MuseException 및 전역 예외 처리(GlobalExceptionHandler) 구현
    - 데이터 소스 라우팅(RoutingDataSource) 및 다중 데이터 소스 설정 추가
    - Logback 설정 파일(local, test, dev, prod) 및 로그 필터(CustomLogbackFilter) 구현
    - Spring Cloud Eureka 클라이언트, OpenFeign, JPA 설정 추가
    - 환경별(application-local, test, dev, prod) 설정 파일 추가
    - 공통적인 ErrorResponse와 CommonDateEntity 생성

--- src/main/java/muse/back/service/MuseBackServiceApplication.java
+++ src/main/java/muse/back/service/MuseBackServiceApplication.java
@@ -2,12 +2,17 @@ package muse.back.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableFeignClients(basePackages = "auth.common.core.client")
+@EnableDiscoveryClient
 public class MuseBackServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MuseBackServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MuseBackServiceApplication.class, args);
+    }
 
 }
