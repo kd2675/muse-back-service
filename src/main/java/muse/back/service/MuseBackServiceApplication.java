d825f395c9f54faf382674cb5dcaa222986f3d6e
김도영 <zz01064842675@gmail.com>
2026-02-03 09:49:55 +0900

    feat: muse-back-service 프로젝트 초기 설정 추가

---
+++ src/main/java/muse/back/service/MuseBackServiceApplication.java
@@ -0,0 +1,13 @@
+package muse.back.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseBackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseBackServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/muse/back/service/MuseBackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package muse.back.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseBackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
